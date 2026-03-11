05e821a7cd455f6d51b9d85fe86fabf3bfa5375d
Juanchope12 <jgiuri@alumno.huergo.edu.ar>
2026-03-11 09:53:18 -0300

    son

--- 9.1.py
+++ 9.1.py
@@ -1,9 +1,12 @@
-lugares = ["paris", "little saint james", "monserrat", "LA", "austria"]
+lugares = ["paris", "little saint james", "monserrat", "la", "austria"]
 def main():
     imprimir()
 
 def imprimir():
     print(lugares)
-    print(f"la lista ordenada es {sorted(lugares)}")
+    lista_ordenada = sorted(lugares)
+    print(f"la lista ordenada es {lista_ordenada}")
+    lista_ordenada_reves = sorted(lista_ordenada, reverse = True)
+    print(f"la lista ordenada al reves es {lista_ordenada_reves}")
 
 main()
