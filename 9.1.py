288537d54b2d6faf9b409ca0bbc24ededc1b9a09
Juanchope12 <jgiuri@alumno.huergo.edu.ar>
2026-03-11 08:47:01 -0300

    calle epro elegante

---
+++ 9.1.py
@@ -0,0 +1,9 @@
+lugares = ["paris", "little saint james", "monserrat", "LA", "austria"]
+def main():
+    imprimir()
+
+def imprimir():
+    print(lugares)
+    print(f"la lista ordenada es {sorted(lugares)}")
+
+main()
